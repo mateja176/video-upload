e255e781edfe2df4a1b40524733ff28318c8ce68
Mateja <mateja176@gmail.com>
2021-02-24 16:53:09 +0100

    installs missing libs

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
 const fetch = require('isomorphic-fetch');
-const Blob = require('blob');
+const Blob = require('node-blob');
+const FormData = require('form-data');
 const { promises: fs } = require('fs');
 const { join } = require('path');
 
 const [uri] = process.argv.slice(2);
 
-fs.readFile(join(__dirname, 'video.webm')).then((buffer) => {
-  const blob = new Blob(buffer, { type: 'video/webm' });
+console.log('Reading file');
+fs.readFile(join(__dirname, 'video.webm'))
+  .then((buffer) => {
+    const blob = new Blob(buffer, { type: 'video/webm' });
 
-  const formData = new FormData();
+    const formData = new FormData();
 
-  formData.append('video.webm', blob);
+    formData.append('video.webm', blob);
 
-  fetch(uri, {
-    method: 'POST',
-    body: formData,
-  })
-    .then((res) => {
-      return res.json();
+    console.log('Uploading file');
+
+    fetch(uri, {
+      method: 'POST',
+      body: formData,
     })
-    .then(console.log);
-});
+      .then((res) => {
+        return res.json();
+      })
+      .then((json) => {
+        console.log('File uploaded');
+        console.log('Response:');
+        console.log(json);
+      });
+  })
+  .catch((error) => {
+    console.log('Failed to upload file');
+    console.log('Error:');
+    console.log(error);
+  });
